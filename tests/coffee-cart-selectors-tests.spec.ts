340463dd7b825af4b87ab1276ffe50fdb5e7538d
Valeriia <vchornogor@gmail.com>
2026-09-23 15:09:53 +0300

    fix: add more locators

--- tests/coffee-cart-selectors-tests.spec.ts
+++ tests/coffee-cart-selectors-tests.spec.ts
@@ -5,17 +5,17 @@ test('valid placing an order', async ({ page }) => {
   
   await page.locator('[aria-label="Espresso"]').click();
   await expect(page.locator('[href="/cart"]')).toContainText('cart (1)');
-  await page.getByLabel('Cappuccino').click();
-  await expect(page.getByRole('link', { name: 'Cart page' })).toContainText('cart (2)');
+  await page.locator('[data-cy="Cappuccino"]').click();
+  await expect(page.locator('[href="/cart"]')).toContainText('cart (2)');
   await page.locator('[data-test="checkout"]').click();
 
-  await expect(page.getByText('Payment details')).toBeVisible();
-  await expect(page.getByRole('textbox', { name: 'Name' })).toBeVisible();
-  await page.getByLabel('Name').fill('test');
-  await expect(page.getByRole('textbox', { name: 'Email' })).toBeVisible();
-  await page.getByLabel('Email').fill('test@email.com');
-  await page.getByRole('button', { name: 'Submit' }).click();
-  await expect(page.getByRole('button', { name: 'Thanks for your purchase.' })).toBeVisible();
+  await expect(page.locator('[aria-label="Payment form"]')).toBeVisible();
+  await expect(page.locator('[id="name"]')).toBeVisible();
+  await page.locator('[id="name"]').fill('test');
+  await expect(page.locator('[id="email"]')).toBeVisible();
+  await page.locator('[id="email"]').fill('test@email.com');
+  await page.locator('[id="submit-payment"]').click();
+  await expect(page.locator('[class="snackbar success"]')).toBeVisible();
 });
 
 test('validate errors on payment form', async ({ page }) => {
@@ -23,41 +23,38 @@ test('validate errors on payment form', async ({ page }) => {
   await page.locator('[aria-label="Espresso"]').click();
   await page.locator('[aria-label="Proceed to checkout"]').click();
 
-  await expect(page.getByRole('heading', { name: 'Payment details' })).toBeVisible();
-  await page.getByRole('textbox', { name: 'Name' }).click();
-  await page.getByLabel('Name').fill('test');
-
-  await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByLabel('Email').fill('test.com');
+  await expect(page.locator('[aria-label="Payment form"]')).toBeVisible();
+  await expect(page.locator('[id="name"]')).toBeVisible();
+  await page.locator('[id="name"]').fill('test');
+  await page.locator('[id="email"]').fill('test.com');
   await page.locator('[id="submit-payment"]').click();
-  await expect(page.getByLabel('Promotion message')).toBeVisible();
 });
 
 test('validate promo after selecting 3 cup', async ({ page }) => {
   await page.goto('/');
   await page.locator('[aria-label="Espresso"]').click();
   await page.locator('[aria-label="Espresso Macchiato"]').click();
-  await expect(page.getByRole('link', { name: 'Cart page' })).toContainText('cart (2)');
+  await expect(page.locator('[href="/cart"]')).toContainText('cart (2)');
   await page.locator('[aria-label="Cappuccino"]').click();
   
-  await expect(page.getByText('It\'s your lucky day! Get an extra cup of Mocha for $4.')).toBeVisible();
-  await page.getByRole('button', { name: 'Yes, of course!' }).click();
-  await expect(page.getByRole('link', { name: 'Cart page' })).toContainText('cart (4)');
+  await expect(page.locator('[class="promo"]')).toBeVisible();
+  await page.locator('[class="yes"]').click();
+  await expect(page.locator('[href="/cart"]')).toContainText('cart (4)');
 });
 
 test('validate cart functionality', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page.getByRole('link', { name: 'Cart page' })).toContainText('cart (0)');
+  await expect(page.locator('[href="/cart"]')).toContainText('cart (0)');
   await page.locator('[data-cy="Cappuccino"]').click();
-  await expect(page.getByRole('link', { name: 'Cart page' })).toContainText('cart (1)');
+  await expect(page.locator('[href="/cart"]')).toContainText('cart (1)');
 
-  await page.getByRole('link', { name: 'Cart page' }).click();
-  await page.getByRole('button', { name: 'Add one Cappuccino' }).click();
-  await expect(page.getByRole('link', { name: 'Cart page' })).toContainText('cart (2)');
+  await page.locator('[href="/cart"]').click();
+  await page.locator('[aria-label="Add one Cappuccino"]').last().click();
+  await expect(page.locator('[href="/cart"]')).toContainText('cart (2)');
 
-  await page.locator('[aria-label="Remove all Cappuccino"]').click();
-  await expect(page.getByText('No coffee, go add some.')).toBeVisible();
+  await page.locator('[class="delete"]').click();
+  await expect(page.locator('p', { hasText: 'No coffee, go add some.' })).toBeVisible();
 });
 
 test('add more coffee by fast menu', async ({ page }) => {
@@ -65,14 +62,13 @@ test('add more coffee by fast menu', async ({ page }) => {
 
   await expect(page.locator('[data-test="Cafe_Breve"]')).toBeVisible();
   await page.locator('[data-test="Cafe_Breve"]').click();
+  await expect(page.locator('[href="/cart"]')).toContainText('cart (1)');
 
   await expect(page.locator('[data-test="checkout"]')).toBeVisible();
   await page.locator('[data-test="checkout"]').hover();
 
-  await expect(page.locator('.cart-preview')).toBeVisible();
-  await page.getByText('Cafe Breve x 1+-').click();
-  await expect(page.getByRole('button', { name: 'Add one Cafe Breve' })).toBeVisible();
-  await page.getByRole('button', { name: 'Add one Cafe Breve' }).click();
+  await expect(page.locator('[aria-label="Add one Cafe Breve"]')).toBeVisible();
+  await page.locator('[aria-label="Add one Cafe Breve"]').click();
 
-  await expect(page.getByRole('link', { name: 'Cart page' })).toBeVisible();
+  await expect(page.locator('[href="/cart"]')).toContainText('cart (2)');
 });
